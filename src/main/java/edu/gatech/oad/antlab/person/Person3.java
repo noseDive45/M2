537c86a857e92beb401ba7d47bc1cee0d6647452
jacksoncook <jacksoncook73@gmail.com>
2018-01-26 09:48:12 -0500

    Modified Person3 to give Person3 a nickname

--- src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -2,15 +2,15 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 3
  *  returns their name and a
- *  reversed string 
- *  
- *  @author  Bob
+ *  nickname
+ *
+ *  @author  Jackson Cook
  *  @version 1.1
  */
 public class Person3 {
-   /** Holds the persons real name */  
+   /** Holds the persons real name */
 	private String name;
-	
+
 	/**
 	 * The constructor, takes in the persons
 	 * name
@@ -19,30 +19,15 @@ public class Person3 {
 	public Person3(String pname){
 	  name = pname;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
-	 * that varies with an input string
+	 * with a nickname
 	 *
-	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
-	public String toString(String input) {
-	  return name + calc(input);
-	}
-	
-	/**
-	 * This method should take the string
-	 * input and return its reverse.
-	 * given "gtg123b" it should return
-	 * b321gtg.
-	 *
-	 * @param input the string to be reversed
-	 * @return the reversed string
-	 */
-	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+	public String toString() {
+	  return name + ": The Bird Man";
 	}
 }
