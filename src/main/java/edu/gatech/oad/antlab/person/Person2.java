20a148a4ea18132cba6a6ad7b0012d741538c20b
Truong, Christopher Taylor <christruong@gatech.edu>
2018-01-29 23:28:51 -0500

    Chris changed this file.

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,11 +1,14 @@
 package edu.gatech.oad.antlab.person;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
- * @author Bob
+ * @author Chris
  * @version 1.1
  */
 public class Person2 {
@@ -30,15 +33,25 @@ public class Person2 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 2 put your implementation here
-	  return null;
+	  List<Character> charArray = new ArrayList<Character>();
+      for (int i = 0; i < input.length(); i++) {
+         charArray.add(input.charAt(i));
+      }
+      Collections.shuffle(charArray);
+
+      String newString = "";
+      for (char c : charArray) {
+        newString += c;
+      }
+
+	  return newString;
 	}
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
