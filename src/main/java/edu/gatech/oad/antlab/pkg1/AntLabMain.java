d83e8021a76d2d5bcd16b8f894334cd0f6828839
Josh Philliber <joshphilliber@gatech.edu>
2018-02-01 01:31:36 -0500

    Adding Josh to File

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -71,10 +71,10 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("Tim Proctor");
                   toPrint += p4.toString("tproctor6");
-                  //Person5 replace P4 with your name
+                  //Person5 replace P5 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Josh Philliber");
+                  toPrint += p5.toString("jphilliber3");
 
         System.out.println(toPrint);
 
